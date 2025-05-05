237f1b8e6f39959b4b2e75ef9f4718f36667b45c
David Le <david29101000@gmail.com>
2025-05-05 17:05:53 +0200

    minor adjustments

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -4,6 +4,9 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+  },
   eslint.configs.recommended,
   tseslint.configs.recommended,
 );
