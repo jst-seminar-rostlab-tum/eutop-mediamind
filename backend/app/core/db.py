2051454a120f6495bead6f480df9d0a7ab649714
carlobortolan <carlobortolan@gmail.com>
2025-05-10 19:14:22 +0200

    fix: revert db.py

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,13 +1,17 @@
-import os
-from dotenv import load_dotenv
-from supabase import Client, create_client
 from config import Configs
+from supabase import Client, create_client
 
 
 def get_supabase_client(cfg: Configs) -> Client:
     if not cfg.SUPABASE_URL or not cfg.SUPABASE_KEY:
-        raise Exception("Supabase credentials are not set")
+        raise Exception(
+            status_code=500, detail="Supabase credentials are not set"
+        )
     try:
-        return create_client(cfg.SUPABASE_URL, cfg.SUPABASE_KEY)
+        supabase: Client = create_client(cfg.SUPABASE_URL, cfg.SUPABASE_KEY)
+        return supabase
     except Exception as e:
-        raise Exception(f"Failed to initialize Supabase client: {str(e)}")
+        raise Exception(
+            status_code=500,
+            detail=f"Failed to initialize Supabase client: {str(e)}",
+        )
