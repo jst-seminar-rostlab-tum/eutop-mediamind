0c186859b9cb5c5b76e400645c1495ba40ca1ce1
carlobortolan <carlobortolan@gmail.com>
2025-05-12 22:06:24 +0200

    fix: update isort config

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -7,7 +7,6 @@ from psycopg2.extensions import connection as PgConnection
 def get_postgresql_connection(cfg: Configs) -> PgConnection:
     if not cfg.DATABASE_URL:
         raise ValueError("DATABASE_URL not set in config.")
-
     try:
         return psycopg2.connect(dsn=cfg.DATABASE_URL)
     except OperationalError as e:
