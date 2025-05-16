c307bd7375605a7e9702ca2e27d8e22ea1f79ef1
A <54219182+dancing-shark@users.noreply.github.com>
2025-05-17 01:00:13 +0200

    feat: refactor configuration management and database initialization

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,34 +1,35 @@
+import sentry_sdk
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
 
-from app.api.v1.routes import routers as v1_routers
-from app.core.config import configs
+from app.api.main import api_router
+from app.core.config import settings
 
 
-class AppCreator:
-    def __init__(self):
-        # set app default
-        self.app = FastAPI(
-            title=configs.PROJECT_NAME,
-            openapi_url="/api/openapi.json",
-            docs_url="/api/docs",
-            version="0.0.1",
-        )
-
-        # set cors
-        if configs.BACKEND_CORS_ORIGINS:
-            self.app.add_middleware(
-                CORSMiddleware,
-                allow_origins=[
-                    str(origin) for origin in configs.BACKEND_CORS_ORIGINS
-                ],
-                allow_credentials=True,
-                allow_methods=["*"],
-                allow_headers=["*"],
-            )
-
-        self.app.include_router(v1_routers, prefix="/api/v1")
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.tags[0]}-{route.name}"
 
 
-app_creator = AppCreator()
-app = app_creator.app
+if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
+    sentry_sdk.init(dsn=str(settings.SENTRY_DSN), enable_tracing=True)
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
+)
+
+# Set all CORS enabled origins
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
+
