59b08be81118694b8251b3ed9fe1dd6a1c6f3aad
Mattia Girolimetto <mgiro2001@gmail.com>
2025-05-19 18:03:47 +0200

    chore: update fe

---
+++ frontend/types/api.ts
@@ -0,0 +1,25 @@
+import createClient from "openapi-fetch";
+import { createQueryHook } from "swr-openapi";
+import type { paths } from "./api-types-v1"
+
+const client = createClient<paths>({
+  baseUrl: "https://myapi.dev/v1/", // TODO change with proper be url
+});
+
+const useQuery = createQueryHook(client, "my-api");
+
+/*
+Example:
+
+function MyComponent() {
+  const { data, error, isLoading, isValidating, mutate } = useQuery(
+    "/api/v1/users"
+  );
+
+  if (isLoading || !data) return "Loading...";
+
+  if (error) return `An error occured: ${error.message}`;
+
+  return <div>{ data.title } </div>;
+}
+*/
