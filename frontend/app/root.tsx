89aed2c2726fe6a7bd2f692468cee47e435d8694
David Le <david29101000@gmail.com>
2025-05-19 16:21:32 +0200

    added hotjar tag

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -32,6 +32,19 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function (c, s, q, u, a, r, e) {
+              c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
+              c._hjSettings = { hjid: a };
+              r = s.getElementsByTagName('head')[0];
+              e = s.createElement('script');
+              e.async = true;
+              e.src = q + c._hjSettings.hjid + u;
+              r.appendChild(e);
+          })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 6408235);`,
+          }}
+        />
       </head>
       <body>
         {children}
