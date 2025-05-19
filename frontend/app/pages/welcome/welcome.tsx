8f05555ddc7f95843c0ba86b99fed16ed986cd53
Leo Mangatia <leo.mangatia@tum.de>
2025-05-19 14:23:13 +0200

    Adjusted layout, added header as component

--- frontend/app/pages/welcome/welcome.tsx
+++ frontend/app/pages/welcome/welcome.tsx
@@ -1,44 +1,43 @@
-import { SquareArrowOutUpRight, User } from "lucide-react";
+import { SquareArrowOutUpRight } from "lucide-react";
 import { Button } from "~/components/ui/button";
+import Header from "~/custom-components/header";
 import Text from "~/custom-components/text";
-
 
 export function Welcome() {
   return (
     <div className="flex flex-col min-h-screen">
+      <Header />
 
-      <div className="p-4 w-full flex justify-between items-center">
-        <img src="/MediaMind_Logo.svg" alt="Logo"/>
-        <Button variant={"white"}>
-          <User/>Login
-        </Button>
-      </div>
-      
-      <img src="Eutop_Wallpaper.svg" alt="Wallpaper" className="w-full object-cover"/>
-      
+      <img
+        src="Eutop_Wallpaper.svg"
+        alt="Wallpaper"
+        className="w-full object-cover"
+      />
+
       <div className="p-8 flex flex-col flex-grow">
         <Text hierachy={1}>Welcome to MediaMind!</Text>
         <p className="p-4 block w-full text-lg leading-relaxed">
-        Stay ahead in the fast-changing world of media with our Automated Press Analysis Tool.
-        Keep your client teams informed and prepared with daily press reviews, personalized to your needs.
-        Our platform extracts and compiles the most relevant news articles into concise, anonymized reports
-         – delivered directly to your inbox each morning.
-        Save time, stay informed, and focus on what matters most – delivering value to your clients.
+          Stay ahead in the fast-changing world of media with our Automated
+          Press Analysis Tool. Keep your client teams informed and prepared with
+          daily press reviews, personalized to your needs. Our platform extracts
+          and compiles the most relevant news articles into concise, anonymized
+          reports – delivered directly to your inbox each morning. Save time,
+          stay informed, and focus on what matters most – delivering value to
+          your clients.
         </p>
 
         <div className="flex justify-evenly p-14 pb-4">
-          <img src="/EUTOP_Logo.svg" alt="EUTOP_Logo"/>
-          <img src="/TUM_Logo.svg" alt="TUM_Logo"/>
+          <img src="/EUTOP_Logo.svg" alt="EUTOP_Logo" />
+          <img src="/TUM_Logo.svg" alt="TUM_Logo" />
         </div>
 
         <p className="flex justify-center">
-          MediaMind was build in cooperation between EUTOP and 
-          the Technical University of Munich during the Javascript Technology Practicum
+          MediaMind was build in cooperation between EUTOP and the Technical
+          University of Munich during the Javascript Technology Practicum
         </p>
-
       </div>
 
-      <div className="p-4 w-full flex justify-end">
+      <div className="p-4 w-full flex justify-center">
         <Button asChild variant="link">
           <span>
             <SquareArrowOutUpRight />
