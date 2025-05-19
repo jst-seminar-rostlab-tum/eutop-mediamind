28b617c0aac059581f1281da09bbf6f464056e86
David Le <david29101000@gmail.com>
2025-05-19 13:45:06 +0200

    feat: added LiteLLM gateway

---
+++ backend/app/services/llm_client/lllm_client.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+from litellm import completion
+
+from backend.app.services.llm_client.llm_models import LLMModels
+
+
+class LLMService:
+    """
+    This takes the LiteLLM Gateway to communicate to any llm provider
+
+    Parameters:
+        model (LLMModels): An enum value representing the language model to
+        use. Extend it if you need it
+        api_key (Optional[str]): API key for authentication. If None, assumes
+        that environment variable is set accordingly
+        https://docs.litellm.ai/docs/
+
+    Examples:
+        >>> service = LLMService(LLMModels.GPT_4)
+        >>> response = service.generate_response("Tell me a joke")
+    """
+
+    def __init__(self, model: LLMModels, api_key: Optional[str] = None):
+        self.model = model.value
+        self.api_key = api_key
+
+    def generate_response(self, prompt: str) -> str:
+        kwargs = {
+            "model": self.model,
+            "messages": [{"role": "user", "content": prompt}],
+        }
+
+        if self.api_key:
+            kwargs["api_key"] = self.api_key
+
+        response = completion(**kwargs)
+        return response
---
+++ backend/app/services/llm_client/llm_models.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class LLMModels(Enum):
+    openai_4o = "gpt-4o"
+    openai_4o_mini = "gpt-4o-mini"
