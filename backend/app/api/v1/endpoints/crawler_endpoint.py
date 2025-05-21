e9fbe748fb7c68ab4ce2bcf81cb3d488eabd9e30
Niklas <s4nibaue@uni-trier.de>
2025-05-21 11:15:33 +0200

    newsapi_ai.py --> Query ready for usage. (limited to 100) for testing!

--- backend/app/api/v1/endpoints/crawler_endpoint.py
+++ backend/app/api/v1/endpoints/crawler_endpoint.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
 
-from app.services.crawler import crawl_urls
+from app.services.crawler import crawl_and_scrape_urls, get_breaking_events
 
 router = APIRouter(prefix="/crawler", tags=["crawler"])
 
 
-@router.get("", response_model=str)
-async def start_crawler():
-    return await crawl_urls()
+@router.get("/get_articles", response_model=str)
+async def start_crawling_and_scraping():
+    return await crawl_and_scrape_urls()
+
+
+@router.get("/get_breaking/events", response_model=list[dict])
+async def start_get_breaking_events():
+    return await get_breaking_events()
