c0cf84b3420d0f8b85c449277fe3bb4edf09fd11
David Le <david29101000@gmail.com>
2025-05-21 15:25:58 +0200

    Revert "cleaned up tag"
    This reverts commit fc32e5bf495e3dac549b0f70a66f7865a1d0ef91.

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -24,8 +24,6 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
-const CONTENTSQUARE_ID = 6408235;
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -36,8 +34,17 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
         {/* Hotjar tracking tag */}
         <script
-          src={`https://static.hj.contentsquare.net/c/csq-${CONTENTSQUARE_ID}.js`}
-          async
+          dangerouslySetInnerHTML={{
+            __html: `(function (c, s, q, u, a, r, e) {
+              c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
+              c._hjSettings = { hjid: a };
+              r = s.getElementsByTagName('head')[0];
+              e = s.createElement('script');
+              e.async = true;
+              e.src = q + c._hjSettings.hjid + u;
+              r.appendChild(e);
+          })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 6408235);`,
+          }}
         />
       </head>
       <body>
