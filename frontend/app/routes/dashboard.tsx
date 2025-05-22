1799d6d3b5d727ff841f98e93e74c9f7ee724088
Rafael Neher <rafineher@gmail.com>
2025-05-22 14:03:33 +0200

    Fix import error

--- frontend/app/routes/dashboard.tsx
+++ frontend/app/routes/dashboard.tsx
@@ -1,11 +1,7 @@
 import { DashboardPage } from "~/pages/dashboard/dashboard";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-const queryClient = new QueryClient();
 
 export default function DashboardRoute() {
   return (
-    <QueryClientProvider client={queryClient}>
       <DashboardPage />
-    </QueryClientProvider>
   );
 }
