3150714000e4e8b5b63819149a036aa8fe1f73dc
Mattia Girolimetto <mgiro2001@gmail.com>
2025-05-23 20:38:40 +0200

    feat: initial terraform structure

---
+++ infra/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "s3-mediamind-tfstate"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+module "ecr" {
+  source = "./modules/ecr"
+  name   = "ecr-mediamind"
+}
