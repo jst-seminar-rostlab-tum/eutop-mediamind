63ef0311534a4de07c8da1ce38d77e82922581e6
Rafael Neher <rafineher@gmail.com>
2025-05-24 16:20:16 +0200

    Change to use Tabs

--- frontend/app/custom-components/profile/edit/topics.tsx
+++ frontend/app/custom-components/profile/edit/topics.tsx
@@ -11,39 +11,54 @@ import {
 } from "~/components/ui/select";
 import { Button } from "~/components/ui/button";
 import { KeywordField } from "~/custom-components/profile/edit/keyword-field";
+import { Label } from "~/components/ui/label";
+import { Input } from "~/components/ui/input";
 
 export function Topics({ profile }: { profile: Profile }) {
   const [selectedTopic, setSelectedTopic] = useState<string | undefined>(
-    undefined,
+    profile.topics.length > 0 ? profile.topics[0].name : undefined,
   );
   const selectedTopicKeywords =
     profile.topics.find((topic) => topic.name === selectedTopic)?.keywords ||
     [];
 
   return (
-    <div className="space-y-4">
-      <Select onValueChange={(value) => setSelectedTopic(value)}>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Select Topic" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Topics</SelectLabel>
-            {profile.topics.map((topic) => (
-              <SelectItem key={topic.name} value={topic.name}>
-                {topic.name}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+    <div>
+      <h2 className={"font-bold pt-3 pb-3"}>Topics and Keywords</h2>
+      <Label className={"text-gray-400 font-normal pb-4"}>
+        Manage topics and keywords for news monitoring
+      </Label>
+      <div className="space-y-4">
+        <div className={"flex gap-3"}>
+          <Label>Topic:</Label>
+          <Select
+            value={selectedTopic}
+            onValueChange={(value) => setSelectedTopic(value)}
+          >
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select Topic" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Topics</SelectLabel>
+                {profile.topics.map((topic) => (
+                  <SelectItem key={topic.name} value={topic.name}>
+                    {topic.name}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <Input
+            placeholder={"+ Add topic"}
+            className={"w-[180px] shadow-none"}
+          />
+        </div>
 
-      {selectedTopic && <KeywordField keywords={selectedTopicKeywords} />}
-      <div className={"flex justify-between"}>
+        {selectedTopic && <KeywordField keywords={selectedTopicKeywords} />}
         {selectedTopic && (
           <Button variant="outline">Delete "{selectedTopic}"</Button>
         )}
-        <Button>Save Changes</Button>
       </div>
     </div>
   );
