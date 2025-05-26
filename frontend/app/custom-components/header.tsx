8d44807cc4f8c22dd3996621cb0a9e5f4c0a901b
Jonathan Ostertag <jo.ostertag@tum.de>
2025-05-26 13:55:37 +0200

    chore: move to authorization hook

--- frontend/app/custom-components/header.tsx
+++ frontend/app/custom-components/header.tsx
@@ -3,32 +3,16 @@ import {
   SignInButton,
   SignedIn,
   UserButton,
-  useUser,
 } from "@clerk/react-router";
 import { User } from "lucide-react";
-import { useEffect } from "react";
-import { useSearchParams } from "react-router";
 import { Button } from "~/components/ui/button";
-
-const NEW_USER_SEARCH_PARAM_NAME = "new_user";
+import {
+  NEW_USER_SEARCH_PARAM_NAME,
+  useAuthorization,
+} from "~/hooks/use-authorization";
 
 export default function Header() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const { isSignedIn, user, isLoaded } = useUser();
-
-  useEffect(() => {
-    if (
-      (searchParams.get(NEW_USER_SEARCH_PARAM_NAME) && isSignedIn && isLoaded,
-      user)
-    ) {
-      console.log(user);
-
-      //TODO: signup at mediamind backend
-
-      searchParams.delete(NEW_USER_SEARCH_PARAM_NAME);
-      setSearchParams(searchParams, { replace: true });
-    }
-  }, [isSignedIn, user, isLoaded]);
+  useAuthorization();
 
   return (
     <div className="p-4 w-full flex justify-between items-center">
---
+++ frontend/app/hooks/use-authorization.tsx
@@ -0,0 +1,26 @@
+import { useUser } from "@clerk/react-router";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router";
+
+export const NEW_USER_SEARCH_PARAM_NAME = "new_user";
+
+// TODO: add mediamind backend request to return role/rights of user (+ within an organization)
+export const useAuthorization = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { isSignedIn, user, isLoaded } = useUser();
+
+  // create user in mediamind backend, when they return from signing up at clerk
+  useEffect(() => {
+    if (
+      (searchParams.get(NEW_USER_SEARCH_PARAM_NAME) && isSignedIn && isLoaded,
+      user)
+    ) {
+      console.log(user);
+
+      //TODO: signup at mediamind backend
+
+      searchParams.delete(NEW_USER_SEARCH_PARAM_NAME);
+      setSearchParams(searchParams, { replace: true });
+    }
+  }, [isSignedIn, user, isLoaded]);
+};
