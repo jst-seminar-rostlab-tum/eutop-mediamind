1e33a4f9346633685d84d0a459ded7089cce5b98
Jonathan Ostertag <jo.ostertag@tum.de>
2025-05-26 13:01:23 +0200

    feat(frontend): setup authentication

--- frontend/app/custom-components/header.tsx
+++ frontend/app/custom-components/header.tsx
@@ -1,14 +1,53 @@
+import {
+  SignedOut,
+  SignInButton,
+  SignedIn,
+  UserButton,
+  useUser,
+} from "@clerk/react-router";
 import { User } from "lucide-react";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router";
 import { Button } from "~/components/ui/button";
 
+const NEW_USER_SEARCH_PARAM_NAME = "new_user";
+
 export default function Header() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { isSignedIn, user, isLoaded } = useUser();
+
+  useEffect(() => {
+    if (
+      (searchParams.get(NEW_USER_SEARCH_PARAM_NAME) && isSignedIn && isLoaded,
+      user)
+    ) {
+      console.log(user);
+
+      //TODO: signup at mediamind backend
+
+      searchParams.delete(NEW_USER_SEARCH_PARAM_NAME);
+      setSearchParams(searchParams, { replace: true });
+    }
+  }, [isSignedIn, user, isLoaded]);
+
   return (
     <div className="p-4 w-full flex justify-between items-center">
       <img src="/MediaMind_Logo.svg" alt="MediaMind_Logo" width={"180px"} />
-      <Button variant={"outline"}>
-        <User />
-        Login
-      </Button>
+      <SignedOut>
+        <Button variant={"outline"} asChild>
+          <span>
+            <User />
+            {/* TODO: dashboard url */}
+            <SignInButton
+              forceRedirectUrl="/"
+              signUpForceRedirectUrl={`/?${NEW_USER_SEARCH_PARAM_NAME}=true`}
+            />
+          </span>
+        </Button>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
