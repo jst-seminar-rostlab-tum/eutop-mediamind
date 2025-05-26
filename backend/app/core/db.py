c2b8e58d3ddbd60b9e5725eb0bcbcee1144de97c
David Le <david29101000@gmail.com>
2025-05-26 14:59:09 +0200

    fix: added logger to db file

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -3,7 +3,11 @@ from psycopg2 import OperationalError
 from psycopg2.extensions import connection as PgConnection
 from qdrant_client import QdrantClient
 
+from app.core.logger import get_logger
+
 from .config import Configs
+
+logger = get_logger(__name__)
 
 
 def get_postgresql_connection(cfg: Configs) -> PgConnection:
@@ -14,6 +18,7 @@ def get_postgresql_connection(cfg: Configs) -> PgConnection:
     except OperationalError as e:
         raise Exception(f"Failed to connect to PostgreSQL database: {str(e)}")
     except Exception as e:
+        logger.error(f"Failed to initialize PostgreSQL client: {str(e)}")
         raise Exception(f"Failed to initialize PostgreSQL client: {str(e)}")
 
 
@@ -23,4 +28,5 @@ def get_qdrant_connection(cfg: Configs) -> QdrantClient:
     try:
         return QdrantClient(url=cfg.QDRANT_URL)
     except Exception as e:
+        logger.error(f"Failed to initialize Qdrant client: {str(e)}")
         raise Exception(f"Failed to initialize Qdrant client: {str(e)}")
