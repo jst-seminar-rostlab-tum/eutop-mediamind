7000f13e4eada03394f0acf3d9c6a0791bedcde9
Davidoof <38215056+davidoof3141@users.noreply.github.com>
2025-05-26 17:25:46 +0200

    Feat: added Qdrant vector database (#74)
    * feat: added Qdrant client & service
    
    * fix: merged docker compose files
    
    * fix: permission error in docker compose. Use named volume instead
    
    * fix: added logger to db file
    
    * fix: added all logger to db

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,15 +1,35 @@
 import psycopg2
-from config import Configs
 from psycopg2 import OperationalError
 from psycopg2.extensions import connection as PgConnection
+from qdrant_client import QdrantClient
+
+from app.core.logger import get_logger
+
+from .config import Configs
+
+logger = get_logger(__name__)
 
 
 def get_postgresql_connection(cfg: Configs) -> PgConnection:
     if not cfg.DATABASE_URL:
+        logger.error("DATABASE_URL not set in config.")
         raise ValueError("DATABASE_URL not set in config.")
     try:
         return psycopg2.connect(dsn=cfg.DATABASE_URL)
     except OperationalError as e:
+        logger.error(f"Failed to connect to PostgreSQL database: {str(e)}")
         raise Exception(f"Failed to connect to PostgreSQL database: {str(e)}")
     except Exception as e:
+        logger.error(f"Failed to initialize PostgreSQL client: {str(e)}")
         raise Exception(f"Failed to initialize PostgreSQL client: {str(e)}")
+
+
+def get_qdrant_connection(cfg: Configs) -> QdrantClient:
+    if not cfg.QDRANT_URL:
+        logger.error("QDRANT_URL not set in config.")
+        raise ValueError("QDRANT_URL not set in config.")
+    try:
+        return QdrantClient(url=cfg.QDRANT_URL)
+    except Exception as e:
+        logger.error(f"Failed to initialize Qdrant client: {str(e)}")
+        raise Exception(f"Failed to initialize Qdrant client: {str(e)}")
