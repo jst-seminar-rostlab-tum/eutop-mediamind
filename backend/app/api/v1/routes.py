a7171c76f68160d72d7fe81c546813933d4f9410
Niklas <s4nibaue@uni-trier.de>
2025-06-01 20:26:52 +0200

    fix(Auth): reformatted

--- backend/app/api/v1/routes.py
+++ backend/app/api/v1/routes.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
-from app.api.v1.endpoints.search_profile_controller import router as search_profile_router
+from app.api.v1.endpoints.search_profile_controller import (
+    router as search_profile_router,
+)
 from app.api.v1.endpoints.user_controller import router as user_router
 
 routers = APIRouter()
