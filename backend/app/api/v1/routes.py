f73f4eb181ff3929d2000d4bd20d56068d57cc56
Niklas <s4nibaue@uni-trier.de>
2025-06-01 16:15:44 +0200

    fix(LiteLLM): test 0.1

--- backend/app/api/v1/routes.py
+++ backend/app/api/v1/routes.py
@@ -1,13 +1,12 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
-from app.api.v1.endpoints.search_profile_controller import (
-    router as search_profile_router,
-)
+from app.api.v1.endpoints.search_profile_controller import router as search_profile_router
 from app.api.v1.endpoints.user_controller import router as user_router
+from app.api.v1.endpoints.auth_controller import router as auth_router
 
 routers = APIRouter()
-router_list = [user_router, search_profile_router]
+router_list = [user_router, auth_router, search_profile_router]
 
 
 @routers.get("/healthcheck", tags=["healthcheck"])
