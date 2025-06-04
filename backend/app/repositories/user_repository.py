6748ecc96985590afd56af3048310f46e6463e0e
Niklas Bauer <nik-bauer@t-online.de>
2025-06-04 14:48:33 +0200

    Fix/authentication (#117)
    * fix(auth): auth.py return internal user
    
    * fix(auth): delete tests (needs to be merged)
    
    * fix(auth): lint
    
    ---------
    
    Co-authored-by: Niklas <s4nibaue@uni-trier.de>

---
+++ backend/app/repositories/user_repository.py
@@ -0,0 +1,45 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+from app.core.db import engine
+from app.models import User
+
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_user_by_clerk_id(clerk_id) -> User:
+    async with async_session() as session:
+        query = select(User).where(User.clerk_id == clerk_id)
+        result = await session.execute(query)
+        return result.scalar_one_or_none()
+
+
+async def update_user(user: User) -> User:
+    async with async_session() as session:
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+        return user
+
+
+async def create_user(user: User) -> User:
+    async with async_session() as session:
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+        return user
+
+
+async def get_user_list_from_organization(user: User) -> list[User] | User:
+    async with async_session() as session:
+        if user.is_superuser:
+            query = select(User)
+        elif user.organization_id is None:
+            return user
+        else:
+            query = select(User).where(
+                User.organization_id == user.organization_id
+            )
+
+        result = await session.execute(query)
+        return result.scalars().all()
