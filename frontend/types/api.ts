f5663b3c775c0b369107fea599371fafc0e8aae6
Jonathan Ostertag <jo.ostertag@tum.de>
2025-06-04 16:15:45 +0200

    fix: env variable

--- frontend/types/api.ts
+++ frontend/types/api.ts
@@ -2,7 +2,8 @@ import createClient from "openapi-fetch";
 import { createQueryHook } from "swr-openapi";
 import type { paths } from "./api-types-v1";
 
-export const BASE_URL = import.meta.env.BACKEND_URL ?? "http://localhost:8000";
+export const BASE_URL =
+  import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8000";
 
 const client = createClient<paths>({
   baseUrl: BASE_URL,
