a07db911bd289370ef2ff64d2eb8fe0e82435d3a
Rafael Neher <101460750+neher42@users.noreply.github.com>
2025-06-04 21:59:28 +0200

    feat: profile edit view (#76)
    * Add profile type
    
    * Add topics and mailing settings
    
    * Add shadcn components
    
    * Add Subscription settings, Improve DataTable Component
    
    * Add ScrollArea
    
    * Change to use Tabs
    
    * Add add mailing dialog
    
    * Add dashboard edit dialog
    
    * Fix could not resolve the import for @radix-ui/react-scroll-area
    
    * Fix could not resolve the import for @radix-ui/react-label
    
    * Fix could not resolve the import for @radix-ui/react-separator
    
    * Remove add mail dialog, Fix scaling issue
    
    * Fix formatting
    
    * Fix formatting
    
    * Add general settings tab
    
    * Add shadcn components
    
    * Fix data-table-subscriptions.tsx, Pin versions
    
    * fix: package-lock
    
    * chore: make editable
    
    ---------
    
    Co-authored-by: Jonathan Ostertag <jo.ostertag@tum.de>
    Co-authored-by: carlobortolan <carlobortolan@gmail.com>

---
+++ frontend/app/custom-components/profile/edit/keyword-tag.tsx
@@ -0,0 +1,23 @@
+import { X } from "lucide-react";
+import { Label } from "~/components/ui/label";
+
+interface KeywordTagProps {
+  name: string;
+}
+
+export function KeywordTag({ name }: KeywordTagProps) {
+  return (
+    <div
+      className={"inline-flex p-1.5 rounded-md items-center gap-1 bg-gray-100"}
+    >
+      <Label>{name}</Label>
+      <div
+        className={
+          "hover:bg-gray-200 transition-colors duration-200 rounded-xs p-0.5"
+        }
+      >
+        <X strokeWidth={"3"} className={"h-3.5 w-3.5 text-gray-400"} />
+      </div>
+    </div>
+  );
+}
---
+++ frontend/app/custom-components/profile/edit/topics.tsx
@@ -0,0 +1,103 @@
+import type { Profile } from "~/types/profile";
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+import { Button } from "~/components/ui/button";
+import { KeywordField } from "~/custom-components/profile/edit/keyword-field";
+import { Label } from "~/components/ui/label";
+import { Input } from "~/components/ui/input";
+import { Trash2 } from "lucide-react";
+
+interface TopicsProps {
+  profile: Profile;
+  setProfile: (profile: Profile) => void;
+}
+
+export function Topics({ profile, setProfile }: TopicsProps) {
+  const [selectedTopic, setSelectedTopic] = useState<string | undefined>(
+    profile.topics.length > 0 ? profile.topics[0].name : undefined,
+  );
+  const [newTopicName, setNewTopicName] = useState("");
+  const selectedTopicKeywords =
+    profile.topics.find((topic) => topic.name === selectedTopic)?.keywords ||
+    [];
+
+  return (
+    <div>
+      <h2 className={"font-bold pt-3 pb-3"}>Topics and Keywords</h2>
+      <Label className={"text-gray-400 font-normal pb-4"}>
+        Manage topics and keywords for news monitoring
+      </Label>
+      <div className="space-y-4">
+        <div className={"flex gap-3"}>
+          <Label>Topic:</Label>
+          <Select
+            value={selectedTopic}
+            onValueChange={(value) => setSelectedTopic(value)}
+          >
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select Topic" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Topics</SelectLabel>
+                {profile.topics.map((topic) => (
+                  <SelectItem key={topic.name} value={topic.name}>
+                    {topic.name}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          {selectedTopic && (
+            <Button
+              variant="destructive"
+              onClick={() => {
+                setProfile({
+                  ...profile,
+                  topics: profile.topics.filter(
+                    (t) => t.name !== selectedTopic,
+                  ),
+                });
+                setSelectedTopic(undefined);
+              }}
+            >
+              <Trash2 />
+              {selectedTopic}
+            </Button>
+          )}
+          <Input
+            value={newTopicName}
+            placeholder={"+ Add topic"}
+            className={"w-[180px] shadow-none ml-auto"}
+            onChange={(e) => setNewTopicName(e.target.value)}
+            onKeyDown={(e) => {
+              if (
+                e.key === "Enter" &&
+                !profile.topics.map((t) => t.name).includes(newTopicName)
+              ) {
+                setProfile({
+                  ...profile,
+                  topics: [
+                    ...profile.topics,
+                    { name: newTopicName, keywords: [] },
+                  ],
+                });
+                setNewTopicName("");
+              }
+            }}
+          />
+        </div>
+
+        {selectedTopic && <KeywordField keywords={selectedTopicKeywords} />}
+      </div>
+    </div>
+  );
+}
