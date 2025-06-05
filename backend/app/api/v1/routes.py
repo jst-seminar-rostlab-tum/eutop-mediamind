71867050374c2cfce261bf62c4a1fec67e782e81
A <54219182+dancing-shark@users.noreply.github.com>
2025-06-05 10:47:25 +0200

    refactor: streamline router list initialization in routes.py

--- backend/app/api/v1/routes.py
+++ backend/app/api/v1/routes.py
@@ -6,12 +6,8 @@ from app.api.v1.endpoints.search_profile_controller import (
 )
 from app.api.v1.endpoints.user_controller import router as user_router
 
-
 routers = APIRouter()
-router_list = [
-    user_router,
-    search_profile_router,
-]
+router_list = [user_router, search_profile_router]
 
 
 @routers.get("/healthcheck", tags=["healthcheck"])
