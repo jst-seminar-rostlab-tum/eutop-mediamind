bff6a6fe0ae5eb740280a2162ec85491c58f408b
Niklas <s4nibaue@uni-trier.de>
2025-06-07 17:12:06 +0200

    feat(endpoints): review @jonathan 0.1 fix

--- backend/app/api/v1/routes.py
+++ backend/app/api/v1/routes.py
@@ -1,22 +1,18 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
-from app.api.v1.endpoints.keyword_controller import router as keyword_router
 from app.api.v1.endpoints.search_profile_controller import (
     router as search_profile_router,
 )
 from app.api.v1.endpoints.subscription_controller import (
     router as subscription_router,
 )
-from app.api.v1.endpoints.topics_controller import router as topics_router
 from app.api.v1.endpoints.user_controller import router as user_router
 
 routers = APIRouter()
 router_list = [
     user_router,
     search_profile_router,
-    topics_router,
-    keyword_router,
     subscription_router,
 ]
 
