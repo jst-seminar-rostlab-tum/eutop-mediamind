e5f7e95144b0689b289c087d584cc202d3950683
Rafael Neher <101460750+neher42@users.noreply.github.com>
2025-06-09 11:35:20 +0200

    Feat/api edit profile view (#153)
    * Fetch profiles
    
    * feat: search-profile email add (but endpoint not working yet)
    
    * Connect Subscriptions API
    
    * Fix linting issue
    
    * Fix linting issue
    
    * Add Toasts and loading indicator
    
    * Fix package-lock issue
    
    * Execute prettier
    
    * Fix typing general.tsx
    
    * Add create profile
    
    * Fix organization_id missing
    
    * feat: improve data handling, updating subs
    
    * chore: make dialog height fixed
    
    * chore: fix cancel
    
    ---------
    
    Co-authored-by: Leo Mangatia <leo.mangatia@tum.de>
    Co-authored-by: Jonathan Ostertag <jo.ostertag@tum.de>

--- frontend/types/model.ts
+++ frontend/types/model.ts
@@ -1,3 +1,11 @@
 import type { components } from "./api-types-v1";
 
 export type MediamindUser = components["schemas"]["User"];
+
+export type Profile = components["schemas"]["SearchProfileDetailResponse"];
+
+export type Subscription = components["schemas"]["SubscriptionSummary"];
+
+export type ProfileUpdate = components["schemas"]["SearchProfileUpdateRequest"];
+
+export type ProfileCreate = components["schemas"]["SearchProfileCreateRequest"];
