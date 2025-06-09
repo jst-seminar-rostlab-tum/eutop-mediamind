e60e8328108d5454200ceabffd3d8d4ae1097584
Niklas <s4nibaue@uni-trier.de>
2025-06-09 20:26:27 +0200

    feat(endpoints):fix async main

--- backend/app/run.py
+++ backend/app/run.py
@@ -1,10 +1,16 @@
 import asyncio
 
-from app.initial_data import main as init_data_main
+from sqlmodel import Session
+
+from app.core.db import engine, init_db
 from app.main import app_creator
 
 
 # Start background tasks here if needed
 @app_creator.app.on_event("startup")
 async def startup_event():
-    asyncio.create_task(init_data_main())
+    asyncio.create_task(init_session())
+
+async def init_session():
+    with Session(engine) as session:
+        await init_db(session)
