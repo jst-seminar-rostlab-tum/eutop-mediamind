8cb8189baa79edd047adbf0078fb44f578dfb40f
Jonathan Ostertag <jo.ostertag@tum.de>
2025-06-10 08:26:26 +0200

    feat(frontend): make clerk a popup

--- frontend/app/custom-components/header.tsx
+++ frontend/app/custom-components/header.tsx
@@ -1,18 +1,50 @@
 import {
   SignedOut,
-  SignInButton,
   SignedIn,
   UserButton,
+  SignIn,
+  SignUp,
 } from "@clerk/react-router";
-import { Link, useSearchParams } from "react-router";
+import { Link, useLocation, useSearchParams } from "react-router";
 import { Settings, User } from "lucide-react";
 import { Button } from "~/components/ui/button";
 import { useAuthorization } from "~/hooks/use-authorization";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
+import { useEffect, useState } from "react";
+
+const SIGN_UP = "ui_sign_up";
+const SIGN_IN = "ui_sign_in";
 
 export default function Header() {
   const { isSignedIn, user } = useAuthorization();
-  const [searchParams] = useSearchParams();
+  const { pathname } = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
   const redirectUrl = searchParams.get("redirect_url");
+  const [openLogin, setOpenLogin] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  useEffect(() => {
+    if (searchParams.get(SIGN_UP) === "true") {
+      setIsSignUp(true);
+      const params = new URLSearchParams(searchParams);
+      params.delete(SIGN_UP);
+      setSearchParams(params);
+    } else if (searchParams.get(SIGN_IN) === "true") {
+      setIsSignUp(false);
+      const params = new URLSearchParams(searchParams);
+      params.delete(SIGN_IN);
+      setSearchParams(params);
+    }
+  }, [searchParams]);
+
+  // Close login on route change as header is always present
+  useEffect(() => {
+    setOpenLogin(false);
+  }, [pathname]);
 
   return (
     <div className="p-4 w-full flex justify-between items-center">
@@ -21,13 +53,36 @@ export default function Header() {
       </Link>
       <div className="col-span-10 flex justify-end gap-2">
         <SignedOut>
-          <SignInButton forceRedirectUrl={redirectUrl ?? "/dashboard"}>
-            <Button asChild variant="outline">
-              <span>
-                <User /> Login
-              </span>
-            </Button>
-          </SignInButton>
+          <Popover
+            open={openLogin}
+            onOpenChange={(open) => {
+              setIsSignUp(false);
+              setOpenLogin(open);
+            }}
+          >
+            <PopoverTrigger asChild>
+              <Button variant="outline">
+                <>
+                  <User /> Login
+                </>
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[25rem] p-0 border-none">
+              {!isSignUp ? (
+                <SignIn
+                  forceRedirectUrl={redirectUrl ?? "/dashboard"}
+                  signUpUrl={pathname + "?" + SIGN_UP + "=true"}
+                  routing="hash"
+                />
+              ) : (
+                <SignUp
+                  forceRedirectUrl={redirectUrl ?? "/dashboard"}
+                  signInUrl={pathname + "?" + SIGN_IN + "=true"}
+                  routing="hash"
+                />
+              )}
+            </PopoverContent>
+          </Popover>
         </SignedOut>
         <SignedIn>
           <UserButton />
