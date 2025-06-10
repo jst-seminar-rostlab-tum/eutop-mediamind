8b6456161fbc0baff239ef8e28dbf17168a34490
Niklas <s4nibaue@uni-trier.de>
2025-06-10 18:30:01 +0200

    feat(alembic): add automated upgrade on startup

--- backend/app/run.py
+++ backend/app/run.py
@@ -2,6 +2,8 @@ import asyncio
 
 from sqlmodel import Session
 
+from alembic import command
+from alembic.config import Config
 from app.core.db import engine, init_db
 from app.main import app_creator
 
@@ -9,7 +11,13 @@ from app.main import app_creator
 # Start background tasks here if needed
 @app_creator.app.on_event("startup")
 async def startup_event():
+    run_migrations()
     asyncio.create_task(init_session())
+
+
+def run_migrations():
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
 
 
 async def init_session():
