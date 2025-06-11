88990cb1c7b2b9065e227ab8914a8eec3f42539d
A <54219182+dancing-shark@users.noreply.github.com>
2025-06-11 23:40:02 +0200

    feat: add match management endpoints for creating matches and retrieving matches by search profile ID

---
+++ backend/app/api/v1/endpoints/match_controller.py
@@ -0,0 +1,43 @@
+from typing import Optional, Sequence, List
+from uuid import UUID
+
+from fastapi import APIRouter, BackgroundTasks, Depends
+
+from app.core.auth import get_authenticated_user
+from app.core.logger import get_logger
+from app.models import Article, Keyword, User, Match
+from app.repositories.keyword_repository import KeywordRepository
+from app.repositories.match_repository import MatchRepository
+from app.services.matching_service import ArticleMatchingService
+
+router = APIRouter(prefix="/matches", tags=["matches"])
+
+logger = get_logger(__name__)
+
+
+@router.post("/create-matches")
+async def create_matches(
+    background_tasks: BackgroundTasks,
+    current_user: User = Depends(get_authenticated_user),
+    page_size: int = 100
+):
+    """
+    Create matches for articles in the background.
+    """
+    if not current_user.is_superuser:
+        logger.warning(f"Unauthorized access attempt by user {current_user.id}")
+        return {"message": "You do not have permission to perform this action."}
+
+    background_tasks.add_task(ArticleMatchingService.run, page_size)
+    return {"message": "Matches are being created in the background."}
+
+@router.get("")
+async def get_matches_by_search_profile(
+    search_profile_id: UUID,
+    current_user: User = Depends(get_authenticated_user)
+) -> List[Match]:
+    """
+    Get matches by search profile ID.
+    """
+    matches: List[Match] = await MatchRepository.get_matches_by_search_profile(search_profile_id, current_user)
+    return matches
--- backend/app/repositories/match_repository.py
+++ backend/app/repositories/match_repository.py
@@ -1,9 +1,11 @@
+from typing import List
 from uuid import UUID
 
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
 
 from app.core.db import async_session
+from app.models import User
 from app.models.match import Match
 
 
@@ -20,6 +22,22 @@ class MatchRepository:
                 .order_by(Match.sorting_order.asc())
             )
             return articles.scalars().all()
+
+    @staticmethod
+    async def get_matches_by_search_profile(
+            search_profile_id: UUID,
+            user: User
+    ) -> List[Match]:
+        async with async_session() as session:
+            query = (
+                select(Match)
+                .where(
+                    Match.search_profile_id == search_profile_id
+                )
+            )
+            matches = (await session.execute(query)).scalars().all()
+
+            return matches
 
     @staticmethod
     async def get_match_by_id(
