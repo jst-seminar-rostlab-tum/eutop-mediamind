4441948a5eb3f6d4cc5964a244f12d0853997089
Niklas <s4nibaue@uni-trier.de>
2025-06-11 19:13:46 +0200

    chore: remove auto migrations

--- backend/app/run.py
+++ backend/app/run.py
@@ -2,8 +2,6 @@ import asyncio
 
 from sqlmodel import Session
 
-from alembic import command
-from alembic.config import Config
 from app.core.db import engine, init_db
 from app.main import app_creator
 
@@ -11,13 +9,7 @@ from app.main import app_creator
 # Start background tasks here if needed
 @app_creator.app.on_event("startup")
 async def startup_event():
-    # run_migrations() should we do auto migrations?
     asyncio.create_task(init_session())
-
-
-def run_migrations():
-    alembic_cfg = Config("alembic.ini")
-    command.upgrade(alembic_cfg, "head")
 
 
 async def init_session():
