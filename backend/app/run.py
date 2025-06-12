16ad2975f35173b3d93ba331cb16c7dd0622f57e
Ricardo Yanzon <ricayanzon@gmail.com>
2025-06-12 18:14:15 +0200

    chore: remove unused paramter and unnecessary function in run and db

--- backend/app/run.py
+++ backend/app/run.py
@@ -2,16 +2,11 @@ import asyncio
 
 from sqlmodel import Session
 
-from app.core.db import engine, init_db
+from app.core.db import init_db
 from app.main import app_creator
 
 
 # Start background tasks here if needed
 @app_creator.app.on_event("startup")
 async def startup_event():
-    asyncio.create_task(init_session())
-
-
-async def init_session():
-    with Session(engine) as session:
-        await init_db(session)
+    asyncio.create_task(init_db())
