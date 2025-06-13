1f4e72d443c26ff57cd7cfee17369e1bdd5a960e
Leo Mangatia <152392510+MagMagnita@users.noreply.github.com>
2025-06-13 11:27:04 +0200

    feat/mock breaking news (#179)
    * removed Breadcrumbs, added new icons and back home button
    
    * Deleted console logs
    
    * Added breaking news to dashboard (mocked news)
    
    * Layout changes breaking news
    
    * fix: color scheme, utility function, version numbers
    
    * removed carousel and its tsx file
    
    * fix: removed grid from layout
    
    ---------
    
    Co-authored-by: Leo Mangatia <leo.mangatia@tum.de>
    Co-authored-by: Jonathan Ostertag <jo.ostertag@tum.de>

--- frontend/app/lib/utils.ts
+++ frontend/app/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function truncateAtWord(text: string, maxLength: number) {
+  if (text.length <= maxLength) return text;
+  const truncated = text.slice(0, maxLength);
+  return truncated.slice(0, truncated.lastIndexOf(" ")) + "…";
+}
