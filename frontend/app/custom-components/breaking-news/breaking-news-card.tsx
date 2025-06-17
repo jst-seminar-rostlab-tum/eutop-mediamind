6a84fe753110a2edd06c756e21beb3583479f24e
Rafael Neher <rafineher@gmail.com>
2025-06-17 22:30:15 +0200

    Linter fix

--- frontend/app/custom-components/breaking-news/breaking-news-card.tsx
+++ frontend/app/custom-components/breaking-news/breaking-news-card.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 
 import type { BreakingNews } from "../../../types/model";
 import { ChevronDown, ChevronUp } from "lucide-react";
