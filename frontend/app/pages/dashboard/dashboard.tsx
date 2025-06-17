537b8643c3f3b50b53c508f477973e145c136178
Rafael Neher <rafineher@gmail.com>
2025-06-17 22:14:33 +0200

    Add breaking news entry dashboard

--- frontend/app/pages/dashboard/dashboard.tsx
+++ frontend/app/pages/dashboard/dashboard.tsx
@@ -1,5 +1,5 @@
 import { ProfileCard } from "~/custom-components/dashboard/profile-card";
-import { Loader2, Plus } from "lucide-react";
+import { Loader2, Megaphone, Plus } from "lucide-react";
 import { Button } from "~/components/ui/button";
 import {
   Breadcrumb,
@@ -14,6 +14,8 @@ import { EditProfile } from "~/custom-components/profile/edit/edit-profile";
 import { sortBy } from "lodash-es";
 import Layout from "~/custom-components/layout";
 import "./dashboard.css";
+import { Alert, AlertTitle } from "~/components/ui/alert";
+import { useNavigate } from "react-router";
 import { useTranslation } from "react-i18next";
 
 const suppressSWRReloading = {
@@ -43,41 +45,8 @@ export function DashboardPage() {
     }
   }, [error]);
 
-  const breakingNews = [
-    {
-      title:
-        "World Bank sharply cuts global growth outlook on trade turbulence",
-      description:
-        "This would mark the slowest rate of global growth since 2008, aside from outright global recessions. It now expects the global economy to expand by 2.3% in 2025, down from an earlier forecast of 2.7%",
-    },
-    {
-      title: "Uber brings forward trialling driverless taxis in UK",
-      description:
-        "The ride-hailing app will work with the UK artificial intelligence (AI) firm Wayve, which has been testing out the technology on the city's streets with human oversight, in line with current legislation.",
-    },
-    {
-      title: "US and China meet for trade talks in London",
-      description:
-        "A senior US delegation including Commerce Secretary Howard Lutnick met Chinese representatives such as Vice Premier He Lifeng at Lancaster House to resolve tension",
-    },
-    {
-      title: "Europe heaps harsh sanctions on Russia",
-      description:
-        "he European Union announced a new package of sanctions against Russia on Tuesday",
-    },
-    {
-      title: "Massive Russian drone attack slams Kyiv",
-      description:
-        "ussia launched 315 drones at Ukraine overnight into Tuesday, in what Ukrainian President Volodymyr Zelensky said was “one of the largest” attacks on the capital Kyiv so far.",
-    },
-    {
-      title:
-        "Greta Thunberg departs Israel on flight to Paris after detention aboard aid ship",
-      description:
-        "Swedish climate and human rights activist Greta Thunberg departed Israel on a flight to France on Tuesday after being detained by Israeli forces",
-    },
-  ];
   const [createDialogOpen, setCreateDialogOpen] = useState(false);
+  const navigate = useNavigate();
 
   return (
     <Layout>
@@ -88,10 +57,14 @@ export function DashboardPage() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      <h1 className="text-3xl font-bold mb-2 mt-1">
-        {t("dashboard.dashboard")}
-      </h1>
-      <BreakingNews breakingNews={breakingNews} />
+      <h1 className="text-3xl font-bold mb-2">Dashboard</h1>
+      <Alert
+        onClick={() => navigate("/dashboard/breaking")}
+        className="hover:bg-gray-100 hover:cursor-pointer mb-2"
+      >
+        <Megaphone />
+        <AlertTitle>Click here to view the latest breaking news!</AlertTitle>
+      </Alert>
       <div className={"flex gap-5"}>
         <h2 className="text-2xl font-bold ">{t("dashboard.profile")}</h2>
         <EditProfile
