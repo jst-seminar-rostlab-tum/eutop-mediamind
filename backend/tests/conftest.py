9c7efe06be4ec5f5c699c83d441e3a655e4ec841
Niklas <s4nibaue@uni-trier.de>
2025-06-17 10:41:10 +0200

    chore: fix tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -16,3 +16,9 @@ def client():
 @pytest.fixture
 def test_name(request):
     return request.node.name
+
+
+@pytest.fixture(autouse=True)
+def use_sqlite_in_tests(monkeypatch):
+    # Before AppCreator is instantiated, force a sqlite URL
+    monkeypatch.setenv("DATABASE_URL", "sqlite:///:memory:")
