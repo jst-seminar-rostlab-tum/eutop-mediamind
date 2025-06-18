bb4a2f081c833e0ace6895e6962a3caddbdc88c0
Rafael Neher <rafineher@gmail.com>
2025-06-18 18:09:41 +0200

    Add error page

--- frontend/app/routes/article.tsx
+++ frontend/app/routes/article.tsx
@@ -1,12 +1,13 @@
 import { ArticlePage } from "~/pages/article/article";
 import { useParams } from "react-router";
 import { getArticle } from "~/pages/article/article-mock-data";
+import { ErrorPage } from "~/pages/error/error";
 
 export default function Article() {
   const { searchProfileId, matchId } = useParams();
 
   if (!searchProfileId || !matchId) {
-    return <div>Missing parameters!</div>;
+    return <ErrorPage />;
   }
 
   const article = getArticle();
