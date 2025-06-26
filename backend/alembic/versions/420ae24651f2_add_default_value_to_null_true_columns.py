5c5cb6d4f4207d77f279f5aecf547f14ffca0f3f
Niklas <nik-bauer@t-online.de>
2025-06-26 13:09:19 +0200

    merge heads

--- backend/alembic/versions/420ae24651f2_add_default_value_to_null_true_columns.py
+++ backend/alembic/versions/420ae24651f2_add_default_value_to_null_true_columns.py
@@ -5,9 +5,6 @@ Create Date: 2025-06-25 19:36:33.800364
 """
 
 from typing import Sequence, Union
-from alembic import op
-import sqlalchemy as sa
-import sqlmodel.sql.sqltypes
 
 # revision identifiers, used by Alembic.
 revision: str = "420ae24651f2"
@@ -18,25 +15,9 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.alter_column(
-        "entities",
-        "id",
-        server_default=sa.text("'00000000-0000-0000-0000-000000000000'"),
-    )
-    op.alter_column(
-        "entities", "entity_type", server_default=sa.text("'unknown'")
-    )
-    op.alter_column("entities", "value", server_default=sa.text("'unknown'"))
-    op.alter_column(
-        "entities",
-        "article_id",
-        server_default=sa.text("'00000000-0000-0000-0000-000000000000'"),
-    )
+    pass
 
 
 def downgrade() -> None:
     """Downgrade schema."""
-    op.alter_column("entities", "id", server_default=None)
-    op.alter_column("entities", "entity_type", server_default=None)
-    op.alter_column("entities", "value", server_default=None)
-    op.alter_column("entities", "article_id", server_default=None)
+    pass
