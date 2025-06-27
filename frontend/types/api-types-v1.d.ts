d289aa842315d4d8be74277f11514b110d336209
API types <ricayanzon@gmail.com>
2025-06-27 17:03:49 +0000

    feat: generated new API types

--- frontend/types/api-types-v1.d.ts
+++ frontend/types/api-types-v1.d.ts
@@ -34,9 +34,14 @@ export interface paths {
          *     return the single user if restricted.
          */
         get: operations["get_users_api_v1_users_get"];
-        put?: never;
+        /** Update Language */
+        put: operations["update_language_api_v1_users_put"];
         post?: never;
-        delete?: never;
+        /**
+         * Delete Current User
+         * @description Delete the authenticated user's account.
+         */
+        delete: operations["delete_current_user_api_v1_users_delete"];
         options?: never;
         head?: never;
         patch?: never;
@@ -56,11 +61,7 @@ export interface paths {
         get: operations["get_current_user_info_api_v1_users_me_get"];
         put?: never;
         post?: never;
-        /**
-         * Delete Current User
-         * @description Delete the authenticated user's account.
-         */
-        delete: operations["delete_current_user_api_v1_users_me_delete"];
+        delete?: never;
         options?: never;
         head?: never;
         patch?: never;
@@ -792,6 +793,11 @@ export interface components {
              * Format: uuid
              */
             owner_id: string;
+            /**
+             * Language
+             * @default en
+             */
+            language: string;
             /** Topics */
             topics: components["schemas"]["TopicCreateOrUpdateRequest"][];
         };
@@ -825,6 +831,11 @@ export interface components {
             owner_id: string;
             /** Is Owner */
             is_owner: boolean;
+            /**
+             * Language
+             * @default en
+             */
+            language: string;
             /** Topics */
             topics: components["schemas"]["TopicResponse"][];
             /** Subscriptions */
@@ -865,6 +876,11 @@ export interface components {
              * Format: uuid
              */
             owner_id: string;
+            /**
+             * Language
+             * @default en
+             */
+            language: string;
             /** Topics */
             topics: components["schemas"]["TopicCreateOrUpdateRequest"][];
         };
@@ -936,6 +952,11 @@ export interface components {
             last_name: string;
             /** Is Superuser */
             is_superuser: boolean;
+            /**
+             * Language
+             * @default en
+             */
+            language: string;
             /** Organization Id */
             organization_id?: string | null;
             /** Organization Name */
@@ -999,6 +1020,57 @@ export interface operations {
             };
         };
     };
+    update_language_api_v1_users_put: {
+        parameters: {
+            query: {
+                language: string;
+            };
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Successful Response */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["UserEntity"];
+                };
+            };
+            /** @description Validation Error */
+            422: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["HTTPValidationError"];
+                };
+            };
+        };
+    };
+    delete_current_user_api_v1_users_delete: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Successful Response */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["FeedbackResponse"];
+                };
+            };
+        };
+    };
     get_current_user_info_api_v1_users_me_get: {
         parameters: {
             query?: never;
@@ -1015,26 +1087,6 @@ export interface operations {
                 };
                 content: {
                     "application/json": components["schemas"]["UserEntity"];
-                };
-            };
-        };
-    };
-    delete_current_user_api_v1_users_me_delete: {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        requestBody?: never;
-        responses: {
-            /** @description Successful Response */
-            200: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": components["schemas"]["FeedbackResponse"];
                 };
             };
         };
