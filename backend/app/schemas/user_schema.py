b653f91404c8844c8b60dd7a654e40eece3fd673
Niklas Bauer <nik-bauer@t-online.de>
2025-06-27 19:00:42 +0200

    Feat/add language to search profile and user (#279)
    * chore: delete run and include into main
    
    * chore: main.py isort
    
    * chore: refactor user and auth files
    
    * chore: fix and refactor search profiles
    
    * chore: fix and refactor search profiles lint
    
    * chore: fix tests
    
    * chore: fix tests
    
    * chore: fix tests
    
    * chore: fix tests
    
    * fix main.py startup procedure for tests.
    
    * fix auth test and set DISABLE_AUTH = False for tests
    
    * fix update and delete unused functions.
    
    * delete unnecessary stuff and add DISABLE_AUTH = False for tests
    
    * lint # noqa
    
    * review
    
    * added can_read can_edit fields to db
    
    * edited the schemas
    
    * add to creation and update process as well as in building the response.
    
    * fixing user endpoints -> fixes after merge.
    
    * fix merge
    
    * add readers and editors to response
    
    * chore: add language to user and SP
    
    * alembic files
    
    * review and fix
    
    * git add almenbic merge
    
    * merging heads
    
    * merge heads
    
    ---------
    
    Co-authored-by: Niklas <s4nibaue@uni-trier.de>

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -16,5 +16,6 @@ class UserEntity(BaseModel):
     first_name: str
     last_name: str
     is_superuser: bool
+    language: str = "en"
     organization_id: Optional[uuid.UUID] = None
     organization_name: Optional[str] = None
