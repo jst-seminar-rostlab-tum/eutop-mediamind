e5c18f467a54e75fd870807bf300d746eb1a29f9
Jonathan Ostertag <jo.ostertag@tum.de>
2025-06-27 20:40:51 +0200

    fix: break long profile names

--- frontend/app/custom-components/profile/edit/edit-profile.tsx
+++ frontend/app/custom-components/profile/edit/edit-profile.tsx
@@ -242,8 +242,8 @@ export function EditProfile({
                   </Button>
                 </div>
               ) : (
-                <div className="flex items-center gap-2">
-                  <DialogTitle className={"text-xl"}>
+                <div className="flex items-center gap-2 w-full">
+                  <DialogTitle className={"text-xl w-full break-all"}>
                     {isCreating
                       ? t("edit_profile.create")
                       : t("edit_profile.edit")}
