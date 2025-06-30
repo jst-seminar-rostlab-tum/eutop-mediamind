15901ad8d4ef05d7d000ff62ed09420799ed5e9d
Rafael Neher <rafineher@gmail.com>
2025-06-30 17:48:33 +0200

    Add link to original article

--- frontend/app/custom-components/breaking-news/breaking-news-card.tsx
+++ frontend/app/custom-components/breaking-news/breaking-news-card.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { ChevronDown, ChevronUp, ExternalLink } from "lucide-react";
 import { formatDate } from "~/lib/utils";
 import { Button } from "~/components/ui/button";
 import type { BreakingNewsItem } from "../../../types/model";
+import { useTranslation } from "react-i18next";
 
 interface NewsCardProps {
   news: BreakingNewsItem;
@@ -13,6 +14,7 @@ export function BreakingNewsCard({ news }: NewsCardProps) {
   const [isExpanded, setIsExpanded] = useState(false);
   const maxLength = 142;
 
+  const { t } = useTranslation();
   const shouldTruncate = news.summary!.length > maxLength;
   const displayText = isExpanded
     ? news.summary
@@ -34,20 +36,32 @@ export function BreakingNewsCard({ news }: NewsCardProps) {
             {formatDate(formatDate(news.published_at!))}
           </p>
           <p className="mb-2">{displayText}</p>
-          {shouldTruncate && (
-            <Button
-              variant="secondary"
-              onClick={() => setIsExpanded(!isExpanded)}
-              className=" font-medium p-1 h-auto"
-            >
-              {isExpanded ? "Show less" : "Read more"}
-              {isExpanded ? (
-                <ChevronUp className="w-4 h-4 ml-2" />
-              ) : (
-                <ChevronDown className="w-4 h-4 ml-2" />
-              )}
-            </Button>
-          )}
+          <div className={"flex gap-4"}>
+            {shouldTruncate && (
+              <Button
+                variant="secondary"
+                onClick={() => setIsExpanded(!isExpanded)}
+                className=" font-medium p-1 h-auto"
+              >
+                {isExpanded
+                  ? t("breaking-news.show_less")
+                  : "breaking-news.show_more"}
+                {isExpanded ? (
+                  <ChevronUp className="w-4 h-4 ml-2" />
+                ) : (
+                  <ChevronDown className="w-4 h-4 ml-2" />
+                )}
+              </Button>
+            )}
+            {news.url && (
+              <Button asChild>
+                <a href={news.url}>
+                  {t("breaking-news.original")}
+                  <ExternalLink />
+                </a>
+              </Button>
+            )}
+          </div>
         </div>
       </div>
     </div>
