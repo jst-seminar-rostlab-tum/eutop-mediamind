b13104a73dbd439e41a06726f7141135072d54cb
Jonathan Ostertag <jo.ostertag@tum.de>
2025-07-07 13:04:07 +0200

    chore: fix percentage (#375)
    * chore: fix percentage
    
    * chore: lint
    
    * chore: add also for relevance

--- frontend/app/pages/search-profile/search-profile.tsx
+++ frontend/app/pages/search-profile/search-profile.tsx
@@ -14,7 +14,11 @@ import { Card, CardTitle } from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
 import Layout from "~/custom-components/layout";
 import Text from "~/custom-components/text";
-import { getLocalizedContent, truncateAtWord } from "~/lib/utils";
+import {
+  getLocalizedContent,
+  getPercentage,
+  truncateAtWord,
+} from "~/lib/utils";
 import { useNavigate } from "react-router";
 import { SidebarFilter } from "./sidebar-filter";
 import { ScrollArea } from "~/components/ui/scroll-area";
@@ -244,14 +248,16 @@ export function SearchProfileOverview() {
                               <div
                                 className={`rounded-lg py-1 px-2 ${bgColor}`}
                               >
-                                Relevance: {relevance}
+                                Relevance: {getPercentage(relevance)}
                               </div>
                               {match.topics.map((topic) => (
                                 <div
                                   className="bg-secondary rounded-lg py-1 px-2"
                                   key={topic.id}
                                 >
-                                  {topic.score + " " + topic.name}
+                                  {getPercentage(topic.score) +
+                                    " " +
+                                    topic.name}
                                 </div>
                               ))}
                             </div>
