12ce506f1a417070f88348cff7738000537518a7
carlobortolan <carlobortolan@gmail.com>
2025-07-07 23:51:07 +0200

    fix: increase ECS task memory from 512 to 2048

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -90,7 +90,7 @@ resource "aws_ecs_task_definition" "app" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
-  memory                   = "512"
+  memory                   = "2048"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   container_definitions = jsonencode([
     {
