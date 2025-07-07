16053c4970f37e69bc2a2ffa739420efffcaf30f
Jonathan Ostertag <jo.ostertag@tum.de>
2025-07-07 19:26:08 +0200

    chore: theme

--- frontend/app/custom-components/dashboard/profile-card.tsx
+++ frontend/app/custom-components/dashboard/profile-card.tsx
@@ -146,7 +146,7 @@ export function ProfileCard({
               />
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                  <Button variant="secondary" className="h-8 w-8 p-0 ">
+                  <Button variant="ghost" className="h-8 w-8 p-0 ">
                     <span className="sr-only">{t("sr-only.open_menu")}</span>
                     <MoreVertical className="h-4 w-4" />
                   </Button>
