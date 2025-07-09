0132a0f9de2ae4c24e56295c9f6789a1697472ca
A <54219182+dancing-shark@users.noreply.github.com>
2025-07-09 21:30:36 +0200

    fix QdrantClient

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -66,10 +66,7 @@ def get_qdrant_connection() -> QdrantClient:
 
     try:
         if configs.ENVIRONMENT == "local":
-            client = QdrantClient(
-                url=configs.QDRANT_URL,
-                api_key=configs.QDRANT_API_KEY,
-            )
+            client = QdrantClient(host="localhost", port=6333)
         else:
             client = QdrantClient(
                 url=configs.QDRANT_URL,
