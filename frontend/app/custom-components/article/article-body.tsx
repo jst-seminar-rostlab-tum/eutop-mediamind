0dc15d1b5e09b855d82bd0d45283268b7b2515a1
Rafael Neher <rafineher@gmail.com>
2025-07-11 16:18:09 +0200

    Add markdown support article page

--- frontend/app/custom-components/article/article-body.tsx
+++ frontend/app/custom-components/article/article-body.tsx
@@ -1,4 +1,5 @@
 import { Calendar, FileClock, User } from "lucide-react";
+import ReactMarkdown from "react-markdown";
 
 interface ArticleBodyProps {
   title: string;
@@ -48,8 +49,8 @@ export function ArticleBody({
           {readingTime}
         </div>
       </div>
-      <article className={"text-gray-800 whitespace-pre-wrap"}>
-        {content}
+      <article className={"prose prose-gray max-w-none"}>
+        <ReactMarkdown>{content}</ReactMarkdown>
       </article>
     </div>
   );
