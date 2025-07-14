5bce1db39c74f7f63b4a624c40259d032a661344
Leo Mangatia <leo.mangatia@tum.de>
2025-07-14 12:54:18 +0200

    fix: added images

--- apps/frontend/app/pages/welcome/welcome.tsx
+++ apps/frontend/app/pages/welcome/welcome.tsx
@@ -93,7 +93,7 @@ export function Welcome() {
         <div className="w-[60%] mx-auto pt-10 pb-4">
           <div className="grid grid-cols-3 place-items-center mt-16 mb-6">
             <img src="/EUTOP_Logo.png" alt="EUTOP_Logo" width={"200px"} />
-            <img src="/TUM_Logo.svg" alt="TUM_Logo" />
+            <img src="/TUM_Logo.png" alt="TUM_Logo" />
             <img src="/csee-logo.webp" alt="CSEE Logo" className="h-20" />
           </div>
           <div>
