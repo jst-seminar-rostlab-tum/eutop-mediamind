db84406ad50cce104cfda6f3a98ead6ff2ba297c
carlobortolan <carlobortolan@gmail.com>
2025-07-16 17:00:03 +0200

    fix: update ALB target group ARN for Qdrant module

--- infra/main.tf
+++ infra/main.tf
@@ -143,7 +143,7 @@ module "qdrant" {
   vpc_id                = data.aws_vpc.selected.id
   vpc_cidr_block        = data.aws_vpc.selected.cidr_block
   api_key               = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["QDRANT_API_KEY"]
-  alb_target_group_arn  = module.alb.alb_target_group_arn_prod
+  alb_target_group_arn  = module.alb.alb_target_group_arn_qdrant
   alb_listener_arn      = module.alb.alb_listener_arn
   alb_security_group_id = module.alb.alb_security_group_id
 }
