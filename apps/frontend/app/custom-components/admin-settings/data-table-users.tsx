0633f4a995c173d8d70c61df3feb37c4aed7e7cc
Leo Mangatia <leo.mangatia@tum.de>
2025-07-16 15:30:52 +0200

    fix: prettier

--- apps/frontend/app/custom-components/admin-settings/data-table-users.tsx
+++ apps/frontend/app/custom-components/admin-settings/data-table-users.tsx
@@ -82,7 +82,6 @@ export function DataTableUsers<TData, TValue>({
 
   return (
     <>
-
       <div className="flex justify-between gap-4 mb-4">
         <Input
           placeholder={"Filter " + t("general.Users")}
