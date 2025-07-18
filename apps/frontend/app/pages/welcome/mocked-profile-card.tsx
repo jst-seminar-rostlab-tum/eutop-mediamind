2c1ea997a819076776c5437fa9eaf22369011833
Leo Mangatia <152392510+MagMagnita@users.noreply.github.com>
2025-07-19 00:26:05 +0200

    Feat/landing page v2 (#493)
    * fix: new branch landing page
    
    * fix: added images
    
    * fix: css scroll and picture display
    
    * feat: mocked topics and sp overview
    
    * feat: functional mocked filter
    
    * fix: translation
    
    * fix: adjusted zooming of cards
    
    * feat: Mocked Article preview and translation
    
    * feat: Motion carousel
    
    * feat: added stepper bar
    
    * fix: changed app css back and made site scrollable
    
    * padding
    
    * Feat: included Mocked Mail and new colors and animations
    
    * fix: made landing_page responsive
    
    * feat: added mcoked breaking news
    
    * fix: breaking news translation
    
    * fix: moved mock data to seperate file
    
    * fix: localized dashboard
    
    * fix: pinned versions
    
    * fix: translated profile card
    
    * fix: translated breaking mock news
    
    * fix: refactored matches mock data for preview
    
    * fix: moved more mock data and changed hero text
    
    * fix: adjusted layout and buttons
    
    * fix: UI and time intervalls
    
    * deleted shadcn carousel
    
    * fix: updated to changes form master (i18n, image in article page)
    
    * fix: typo in translation
    
    * fix: UI, links and buttons
    
    ---------
    
    Co-authored-by: Leo Mangatia <leo.mangatia@tum.de>

---
+++ apps/frontend/app/pages/welcome/mocked-profile-card.tsx
@@ -0,0 +1,183 @@
+import {
+  Trash2,
+  SquarePen,
+  MoreVertical,
+  ChevronRight,
+  FileText,
+} from "lucide-react";
+import { useState, useRef, useEffect } from "react";
+import { Button } from "~/components/ui/button";
+import type { Profile } from "../../../types/model";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { useTranslation } from "react-i18next";
+import { RoleBadge } from "~/custom-components/dashboard/role-badge";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
+import { toast } from "sonner";
+
+interface ProfileCardProps {
+  profile: Profile;
+  profile_id: string;
+}
+
+export function MockedProfileCard({ profile, profile_id }: ProfileCardProps) {
+  const [isTruncated, setIsTruncated] = useState(false);
+  const titleRef = useRef<HTMLHeadingElement>(null);
+
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    const element = titleRef.current;
+    const handleResize = () => {
+      if (element) {
+        setIsTruncated(element.scrollWidth > element.clientWidth);
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [profile.name]);
+
+  const getRoleBadge = () => {
+    if (profile.owner_id === profile_id) {
+      return <RoleBadge variant={"owner"} />;
+    } else if (profile.can_edit_user_ids.includes(profile_id)) {
+      return <RoleBadge variant={"editor"} />;
+    } else if (profile.can_read_user_ids.includes(profile_id)) {
+      return <RoleBadge variant={"reader"} />;
+    }
+    return null;
+  };
+
+  const getVisibilityBadge = () => {
+    if (profile.is_public) {
+      return <RoleBadge variant={"public"} />;
+    }
+
+    const totalUsersWithAccess = new Set([
+      ...profile.can_read_user_ids,
+      ...profile.can_edit_user_ids,
+    ]).size;
+
+    if (
+      totalUsersWithAccess === 0 ||
+      (totalUsersWithAccess === 1 && profile.owner_id === profile_id)
+    ) {
+      return <RoleBadge variant={"private"} />;
+    }
+
+    return <RoleBadge variant={"shared"} />;
+  };
+
+  const totalKeywords = profile.topics.flatMap(
+    (topic) => topic.keywords,
+  ).length;
+
+  return (
+    <TooltipProvider>
+      <div className="w-[15.5rem] h-[14rem] rounded-3xl shadow-[2px_2px_15px_rgba(0,0,0,0.1)] p-5 ">
+        <div className="h-full flex-1 flex flex-col justify-between overflow-hidden">
+          <div>
+            <div className={"flex justify-between"}>
+              <div className={"flex items-center gap-2"}>
+                <Tooltip open={isTruncated ? undefined : false}>
+                  <TooltipTrigger asChild>
+                    <h2
+                      ref={titleRef}
+                      className={`font-semibold text-xl min-w-0 flex-1 truncate ${
+                        profile.new_articles_count > 0
+                          ? "max-w-[140px]"
+                          : "max-w-[160px]"
+                      }`}
+                    >
+                      {profile.name}
+                    </h2>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{profile.name}</p>
+                  </TooltipContent>
+                </Tooltip>
+                {profile.new_articles_count > 0 && (
+                  <span
+                    className={`bg-red-200 ${profile.new_articles_count > 99 ? "w-8" : "w-5"} h-5 rounded-full flex items-center justify-center text-sm font-bold text-red-900`}
+                  >
+                    {profile.new_articles_count}
+                  </span>
+                )}
+              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost" className="h-8 w-8 p-0 ">
+                    <span className="sr-only">{t("sr-only.open_menu")}</span>
+                    <MoreVertical className="h-4 w-4" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="center">
+                  <DropdownMenuItem
+                    onClick={() =>
+                      toast.info(t("landing_page.edit_profile_toast"))
+                    }
+                  >
+                    <SquarePen className="text-primary" />
+                    {t("Edit")}
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    onClick={() => toast.info(t("landing_page.reports_toast"))}
+                  >
+                    <FileText className="text-primary" />
+                    {t("search_profile.reports")}
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    onClick={() =>
+                      toast.info(t("landing_page.delete_profile_toast"))
+                    }
+                    className="text-red-500 focus:text-red-500"
+                  >
+                    <Trash2 className="text-red-500" />
+                    {t("Delete")}
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            <div className={"flex gap-2 flex-wrap"}>
+              {getRoleBadge()}
+              {getVisibilityBadge()}
+            </div>
+            <div className={"mt-2 mb-2"}>
+              <div className={"flex items-center gap-1"}>
+                <div className="bg-gray-100 px-1 py-0.5 text-sm rounded-sm text-gray-700 flex gap-1">
+                  <span className={"font-bold"}>{profile.topics.length}</span>
+                  {t("search_profile.Topics")}
+                </div>
+                <div className="bg-gray-100 px-1 py-0.5 text-sm rounded-sm text-gray-700 flex gap-1">
+                  <span className={"font-bold"}>{totalKeywords}</span>
+                  {t("search_profile.Keywords")}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            onClick={() => toast.info(t("landing_page.checkout_sp_toast"))}
+            className="w-full h-20 bg-gray-100 items-center flex justify-center rounded-2xl hover:bg-gray-200 hover:cursor-pointer transition-background duration-300"
+          >
+            <span className={"text-gray-700"}>
+              {t("search_profile.Explore")}
+            </span>
+            <ChevronRight className="w-7 h-7" />
+          </div>
+        </div>
+      </div>
+    </TooltipProvider>
+  );
+}
---
+++ apps/frontend/app/pages/welcome/mocked-search-profile.tsx
@@ -0,0 +1,278 @@
+import { Book, FileText, Search } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Card, CardTitle } from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+import {
+  getLocalizedContent,
+  getPercentage,
+  truncateAtWord,
+} from "~/lib/utils";
+import { MockedSidebarFilter } from "./mocked-sidebar-filter";
+import { ScrollArea, ScrollBar } from "~/components/ui/scroll-area";
+import { useTranslation } from "react-i18next";
+import { Button } from "~/components/ui/button";
+import { toast } from "sonner";
+import { matches } from "./mock-data";
+import i18n from "~/i18n";
+
+export function MockedSearchProfileOverview() {
+  const [search, setSearch] = useState("");
+  const [sortBy, setSortBy] = useState<"relevance" | "date">("relevance");
+
+  const [searchSources, setSearchSources] = useState("");
+
+  const [searchTopics, setSearchTopics] = useState("");
+
+  const [fromDate, setFromDate] = useState<Date>();
+  const [toDate, setToDate] = useState<Date>();
+
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const { t } = useTranslation();
+
+  const profile = {
+    id: "1",
+    name: "Eutop",
+    is_public: true,
+    organization_emails: ["user@example.com"],
+    profile_emails: ["user@example.com"],
+    can_read_user_ids: ["1"],
+    is_reader: true,
+    can_edit_user_ids: ["1"],
+    is_editor: true,
+    owner_id: "1",
+    is_owner: true,
+    language: "en",
+    topics: [
+      {
+        id: "1",
+        name: "Automotive",
+        keywords: ["Tires", "Engine"],
+      },
+      {
+        id: "2",
+        name: "Environment",
+        keywords: ["CO2", "Renewable Energy"],
+      },
+    ],
+    subscriptions: [
+      {
+        id: "1",
+        name: "Spiegel",
+        is_subscribed: true,
+      },
+      {
+        id: "2",
+        name: "Welt",
+        is_subscribed: true,
+      },
+      {
+        id: "3",
+        name: "FAZ",
+        is_subscribed: true,
+      },
+    ],
+    new_articles_count: 3,
+  };
+
+  const [selectedTopics, setSelectedTopics] = useState<string[]>(
+    profile.topics.map((s) => s.id),
+  );
+  const [selectedSources, setSelectedSources] = useState<string[]>(
+    profile.subscriptions.map((s) => s.id),
+  );
+
+  const [filteredMatches, setFilteredMatches] = useState(matches);
+
+  useEffect(() => {
+    const from = fromDate ?? new Date("1900-01-01");
+    const to = toDate ?? new Date("9999-12-31");
+
+    const filtered = matches.filter((match) => {
+      const date = new Date(match.article.date);
+
+      // Date filter
+      if (date < from || date > to) return false;
+
+      // Search term in headline or summary
+      const content =
+        getLocalizedContent(match.article.headline, i18n) +
+        getLocalizedContent(match.article.summary, i18n);
+
+      if (
+        searchTerm &&
+        !content.toLowerCase().includes(searchTerm.toLowerCase())
+      ) {
+        return false;
+      }
+
+      // Topics filter
+      if (
+        selectedTopics.length > 0 &&
+        !match.topics.some((t) => selectedTopics.includes(t.id))
+      ) {
+        return false;
+      }
+
+      // Source filter (based on article.source)
+      if (
+        selectedSources.length == 0 ||
+        !selectedSources.includes(match.article.source)
+      ) {
+        return false;
+      }
+
+      return true;
+    });
+
+    // Sorting
+    const sorted = [...filtered].sort((a, b) => {
+      if (sortBy === "date") {
+        return (
+          new Date(b.article.date).getTime() -
+          new Date(a.article.date).getTime()
+        );
+      } else {
+        return b.relevance - a.relevance;
+      }
+    });
+
+    setFilteredMatches(sorted);
+  }, [fromDate, toDate, sortBy, searchTerm, selectedTopics, selectedSources]);
+
+  const Sources = profile ? profile.subscriptions : [];
+  const Topics = profile ? profile.topics : [];
+
+  return (
+    <div className="w-full grow flex flex-col overflow-hidden pt-3">
+      <div className="w-full flex flex-col justify-start">
+        <div className="flex items-center justify-between mb-4 gap-10">
+          <ScrollArea className="grow overflow-x-hidden whitespace-nowrap rounded-md pb-1.5">
+            <div className="flex w-max space-x-2 p-1">
+              <div className="flex items-center gap-1 shrink-0">
+                <Book size={20} />
+                <p className="font-bold">{t("search_profile.Topics")}</p>
+              </div>
+              {profile?.topics?.map((topic, idx) => (
+                <div
+                  className="bg-gray-200 rounded-lg py-1 px-2 shrink-0"
+                  key={idx}
+                >
+                  {topic.name}
+                </div>
+              ))}
+            </div>
+            <ScrollBar orientation="horizontal" />
+          </ScrollArea>
+          <Button asChild>
+            <span
+              className="inline-flex items-center gap-2"
+              onClick={() => toast.info(t("landing_page.click_reports"))}
+            >
+              <FileText />
+              {t("reports.reports")}
+            </span>
+          </Button>
+        </div>
+      </div>
+
+      <div className="overflow-hidden grow flex flex-row justify-start mt-2 mb-4 gap-8">
+        <div className="max-w-[400px] h-full">
+          <MockedSidebarFilter
+            sortBy={sortBy}
+            setSortBy={setSortBy}
+            selectedSources={selectedSources}
+            setSelectedSources={setSelectedSources}
+            searchSources={searchSources}
+            setSearchSources={setSearchSources}
+            Sources={Sources}
+            selectedTopics={selectedTopics}
+            setSelectedTopics={setSelectedTopics}
+            searchTopics={searchTopics}
+            setSearchTopics={setSearchTopics}
+            Topics={Topics}
+            fromDate={fromDate}
+            setFromDate={setFromDate}
+            toDate={toDate}
+            setToDate={setToDate}
+          />
+        </div>
+        <div className="min-w-[500px] grow flex flex-col overflow-hidden">
+          <div className="relative mb-4 w-full flex">
+            <Input
+              placeholder={t("Search") + " " + t("search_profile.articles")}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <Button
+              variant="default"
+              className="rounded-m ml-2"
+              onClick={() => setSearchTerm(search)}
+            >
+              <Search />
+            </Button>
+          </div>
+          <div className="bg-card rounded-lg border shadow-sm grow overflow-hidden">
+            <div className="h-full">
+              <ScrollArea className="p-4 h-full">
+                {filteredMatches.map((match) => {
+                  const relevance = match.relevance;
+
+                  const bgColor =
+                    relevance > 0.7
+                      ? "bg-green-200"
+                      : relevance < 0.3
+                        ? "bg-red-200"
+                        : "bg-yellow-200";
+
+                  return (
+                    <Card
+                      className="mb-4 p-5 gap-4 justify-start"
+                      key={match.id}
+                    >
+                      <div className="flex flex-row gap-4">
+                        <img
+                          src={match.article.image_urls[0]}
+                          alt={getLocalizedContent(
+                            match.article.headline,
+                            i18n,
+                          )}
+                          className="w-[130px] h-[130px] object-cover rounded-md shadow-md shrink-0"
+                        />
+                        <div className="flex flex-col justify-evenly gap-4 p-2">
+                          <CardTitle className="text-xl">
+                            {getLocalizedContent(match.article.headline, i18n)}
+                          </CardTitle>
+                          <p>
+                            {truncateAtWord(
+                              getLocalizedContent(match.article.summary, i18n),
+                              190,
+                            )}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="flex gap-3 items-center">
+                        <div className={`rounded-lg py-1 px-2 ${bgColor}`}>
+                          {t("search_profile.Relevance")}{" "}
+                          {getPercentage(relevance)}
+                        </div>
+                        {match.topics.map((topic) => (
+                          <div
+                            className="bg-gray-200 rounded-lg py-1 px-2"
+                            key={topic.id}
+                          >
+                            {getPercentage(topic.score) + " " + topic.name}
+                          </div>
+                        ))}
+                      </div>
+                    </Card>
+                  );
+                })}
+              </ScrollArea>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
