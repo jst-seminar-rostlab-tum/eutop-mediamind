3f3f21be49132d3be22f4ecc9070d32a4b1c86b9
Jonathan Ostertag <jo.ostertag@tum.de>
2025-07-21 17:51:15 +0200

    fix: logout redirect

--- apps/frontend/app/hooks/use-authorization.tsx
+++ apps/frontend/app/hooks/use-authorization.tsx
@@ -44,8 +44,13 @@ export const AuthorizationContextProvider = ({
       !pathname.includes("error") &&
       pathname !== "/"
     ) {
+      if (mediamindUser) {
+        // we have been signed in before, so we just logged out, in this case we navigate to the landing page
+        navigate("/");
+      } else {
+        navigate("error/401?redirect_url=" + pathname);
+      }
       setMediamindUser(undefined);
-      navigate("error/401?redirect_url=" + pathname);
     }
   }, [isLoaded, isSignedIn, pathname]);
 
