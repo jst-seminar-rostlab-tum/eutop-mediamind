044e9c13a3563ecb9e00aea64cb2a6073c03f982
A <54219182+dancing-shark@users.noreply.github.com>
2025-07-23 11:10:18 +0200

    fix bug

--- apps/backend/app/services/admin_report_service.py
+++ apps/backend/app/services/admin_report_service.py
@@ -8,9 +8,9 @@ from apps.backend.app.services.crawl_stats_pdf_service import (
     generate_crawl_stats_pdf,
 )
 from apps.backend.app.services.email_service import EmailService
-from app.core.languages import Language
 
 from app.core.config import get_configs
+from app.core.languages import Language
 
 configs = get_configs()
 logger = get_logger(__name__)
@@ -39,22 +39,24 @@ class AdminReportService:
         with open(report_filename, "rb") as f:
             pdf_bytes = f.read()
 
-        if superuser and superuser.language in Language._value2member_map_:
-            translator_language = superuser.language
-        else:
-            translator_language = Language.EN
-
-        content = EmailService._build_admin_report_email_content(
-            superuser.last_name if superuser else None,
-            superuser.gender if superuser else None,
-            translator_language
-        )
-
         for superuser in superusers:
             if not superuser.email:
-                logger.warning(f"Superuser {superuser.id} has no email address, skipping.")
+                logger.warning(
+                    f"Superuser {superuser.id} has no email address, skipping."
+                )
                 continue
-                
+
+            if superuser and superuser.language in Language._value2member_map_:
+                translator_language = superuser.language
+            else:
+                translator_language = Language.EN
+
+            content = EmailService._build_admin_report_email_content(
+                superuser.last_name if superuser else None,
+                superuser.gender if superuser else None,
+                translator_language,
+            )
+
             email = Email(
                 sender=configs.SMTP_USER,
                 recipient=superuser.email,
